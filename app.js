d03869ce8ee4660666d272277a8765969d0d62c8
pktherock <pktherock00@gmail.com>
2021-07-23 14:55:23 +0530

    Adding Analog clock project

---
+++ app.js
@@ -0,0 +1,22 @@
+let secondHand = document.querySelector("#sec");
+let minHand = document.querySelector("#min")
+let hourHand = document.querySelector("#hr")
+// const audio = new audio('')
+
+
+setInterval(clockRotating, 1000)
+
+function clockRotating() {
+    var date = new Date();
+    var getSeconds = date.getSeconds() / 60;
+    var getMinutes = date.getMinutes() / 60;
+    var getHours = date.getHours() / 12
+
+    secondHand.style.transform = "rotate(" + getSeconds * 360 + "deg)"
+    minHand.style.transform = "rotate(" + getMinutes * 360 + "deg)"
+    hourHand.style.transform = "rotate(" + getHours * 360 + "deg)"
+
+    document.querySelector(".current-day").innerHTML = date.toDateString()
+    document.querySelector(".current-seconds").innerHTML = date.getSeconds()
+    audio.play();
+}
