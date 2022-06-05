9e5559525ef412254bbbbea8399b3ef59b0c773c
pktherock <pktherock00@gmail.com>
2022-06-05 20:44:44 +0530

    link added for live demo!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Analog-Clock
+
 ## This is an Analog Clock created by using Html, css, &amp; Js
+
+<a href=" https://pktherock.github.io/Analog-Clock/">Click me for live demo!</a>
--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-let secondHand = document.querySelector("#sec");
-let minHand = document.querySelector("#min")
-let hourHand = document.querySelector("#hr")
+let secondHand = document.querySelector('#sec');
+let minHand = document.querySelector('#min');
+let hourHand = document.querySelector('#hr');
 // const audio = new audio('')
 
-
-setInterval(clockRotating, 1000)
-
 function clockRotating() {
-    var date = new Date();
-    var getSeconds = date.getSeconds() / 60;
-    var getMinutes = date.getMinutes() / 60;
-    var getHours = date.getHours() / 12
+  var date = new Date();
+  var getSeconds = date.getSeconds() / 60;
+  var getMinutes = date.getMinutes() / 60;
+  var getHours = date.getHours() / 12;
 
-    secondHand.style.transform = "rotate(" + getSeconds * 360 + "deg)"
-    minHand.style.transform = "rotate(" + getMinutes * 360 + "deg)"
-    hourHand.style.transform = "rotate(" + getHours * 360 + "deg)"
+  secondHand.style.transform = 'rotate(' + getSeconds * 360 + 'deg)';
+  minHand.style.transform = 'rotate(' + getMinutes * 360 + 'deg)';
+  hourHand.style.transform = 'rotate(' + getHours * 360 + 'deg)';
 
-    document.querySelector(".current-day").innerHTML = date.toDateString()
-    document.querySelector(".current-seconds").innerHTML = date.getSeconds()
-    audio.play();
+  document.querySelector('.current-day').innerHTML = date.toDateString();
+  document.querySelector('.current-seconds').innerHTML = date.getSeconds();
+  audio.play();
 }
+
+setInterval(clockRotating, 1000);
